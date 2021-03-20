6b69dfa233c8207e6a0f397922f5ae8377ad3fa3
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2021-03-20 18:27:13 -0500

    Syntax improvement

--- dishook.go
+++ dishook.go
@@ -16,21 +16,21 @@ func main() {
 	msg := os.Args[2:]
 
 	if argsLen == 1 {
-		fmt.Println("Please provide webhook link, the message and try again.")
-		fmt.Println("Example: dishook https://discord.com/api/webhooks/.../.../ Hello World!")
-		os.Exit(0)
+		panic("Please provide webhook link, the message and try again." +
+			  "Example: dishook https://discord.com/api/webhooks/.../.../ Hello World!")
 		// Checks if there's no URL in first argument
 	}
 	if argsLen == 2 {
 		isDiscordLink(url)
-		fmt.Println("Please put in the message and try again.")
-		os.Exit(0)
+		panic("Please put in the message and try again.")
 		// Check if there's no content in second+ argument
 	}
 
 	// Checks if webhook is valid and has no errors whatsoever.
 	isDiscordLink(url)
 	isTokenInvalid(url)
+	// If one of them are invalid, the script turns off. Refer to the
+	// definition of the features for more information.
 
 	// Process message argument
 	var msgBuf string
@@ -38,7 +38,9 @@ func main() {
 		msgBuf = msgBuf + " " + msg[i]
 	}
 	msgBuf = strings.TrimSpace(msgBuf)
-	// fmt.Println(msgBuf)
+	isMsgMax(msgBuf) // Checks if message surpasses Discord's limit.
+					 // Mesage is not sent if it returns true (refer to
+					 // the definition of the features) 
 
 	// Sends request to webhook
 	sendMsg(url, msgBuf)
@@ -51,7 +53,6 @@ func sendMsg(url string, content string) {
 	// Turns content string into JSON
 	
 	resp, err := requests.Post(url, "application/json", bytes.NewBuffer(jsonValue))
-	fmt.Println("Yeahh")
 	fmt.Print(resp)
 	manageError(err)
 	// Sends request to webhook
@@ -61,20 +62,21 @@ func isDiscordLink(url string) bool {
 	if url[0:33] == "https://discord.com/api/webhooks/" {
 		return true
 	} else {
-		fmt.Println("Please provide a valid webhook URL and try again.")
-		os.Exit(0)
+		panic("Please provide a valid webhook URL and try again.")
 	}
-	return false
 }
 
 func isMsgMax(msg string) bool {
 	msgLen := len(msg)
-	msgLimit := 2000
+	msgLimit := 2000 // you never know if discord may change their 
+					 // limit in the near future /shrug
+	
 	if msgLen < msgLimit {
 		return false
 	} else {
 		msgToShort := msgLen - msgLimit
-		fmt.Printf("Your message's length (%d) surpasses Discord's limit (%d). Please make it %d characters shorter and try again.", 
+		fmt.Printf("Your message's length (%d) surpasses Discord's limit (%d)." +
+		"Please make it %d characters shorter and try again.", 
 		msgLen, msgLimit, msgToShort)
 
 		return true
@@ -82,13 +84,13 @@ func isMsgMax(msg string) bool {
 }
 
 func isTokenInvalid(url string) {
+	// atm it only gets the url's status code. remember, this is a beta.
 	url_r, err := requests.Get(url)
 	manageError(err)
 
 	url_code := url_r.StatusCode // thank you discord for putting the invalid error in the url's status code
 	if url_code == 401 {
-		fmt.Println("Invalid Webhook Token. Please try again")
-		os.Exit(0)
+		panic("Invalid Webhook Token. Please try again")
 	}
 }
 
@@ -96,6 +98,7 @@ func manageError(err error) {
 	// just to calm down with the syntax
 	if err != nil {
 		fmt.Println("An unexpected error ocurred. Please try again.")
+		fmt.Println("For more information:")
 		panic(err)
 	}
 }
