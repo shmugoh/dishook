93e2fd25bc78a77d74cd265f339879f21b290db3
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2021-12-16 00:26:56 -0500

    print json with indent when no flag is parsed

--- cmd/get.go
+++ cmd/get.go
@@ -17,6 +17,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"reflect"
 
 	"github.com/jochasinga/requests"
@@ -40,11 +41,31 @@ var get_cmd = &cobra.Command{
 	// MaximumArgs: cobra.MaximumNArgs(3),
 	// Long: maybe i won't use it, but i'll leave it here just in case
 
-	RunE: func(cmd *cobra.Command, args []string) error {
+	PreRunE: func(cmd *cobra.Command, args []string) error {
 		url := fmt.Sprintf("%s/messages/%s", args[0], args[1])
 		if !is_token_valid(url) {
 			ManageError(fmt.Errorf("'%s' not a valid webhook token", args[0]))
 		}
+
+		if len(args) == 2 {
+			resp_json, err := requests.Get(url)
+			ManageError(err)
+
+			var resp_map map[string]interface{}
+			json.Unmarshal([]byte(resp_json.JSON()), &resp_map)
+			buff, err := json.MarshalIndent(resp_map, "", "  ")
+			ManageError(err)
+
+			fmt.Println(string(buff))
+			os.Exit(0)
+		}
+
+		return nil
+	},
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		url := fmt.Sprintf("%s/messages/%s", args[0], args[1])
+
 		resp_json, err := requests.Get(url)
 		ManageError(err)
 		var resp_map map[string]interface{}
@@ -65,7 +86,6 @@ var get_cmd = &cobra.Command{
 			"components", "edited_timestamp", "embeds", "flags",
 		}
 
-		var is_flag_set bool
 		for i := 0; i < len(flags_map_bool); i++ {
 			if flags_map_bool[i] {
 				output := resp_map[flags_map_var[i]]
@@ -75,13 +95,7 @@ var get_cmd = &cobra.Command{
 				}
 
 				fmt.Printf("%s: %s\n", flags_map_var[i], output)
-				is_flag_set = true // lol
 			}
-		}
-
-		// default if no flag is set
-		if !is_flag_set {
-			fmt.Println(resp_map)
 		}
 		return nil
 	},
