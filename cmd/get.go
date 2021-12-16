178a3eda4ae98d4bb82deacde6cdeef9272c4b81
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2021-12-15 19:06:23 -0500

    fix spelling, rename variables and flag no set

--- cmd/get.go
+++ cmd/get.go
@@ -25,75 +25,54 @@ import (
 
 var get_cmd = &cobra.Command{
 	Use:   "get [URL] [message-id]",
-	Short: "Returns a previously-sent message (or an argument if a flag is used)",
+	Short: "Returns info of a message sent by a webhook",
 	Args:  cobra.MinimumNArgs(2),
 	// MaximumArgs: cobra.MaximumNArgs(3),
 	// Long: maybe i won't use it, but i'll leave it here just in case
 
-	Run: func(cmd *cobra.Command, args []string) {
-		// i might move this to root.go
-		flags := []bool{
+	RunE: func(cmd *cobra.Command, args []string) error {
+		url := fmt.Sprintf("%s/messages/%s", args[0], args[1])
+		if !is_token_valid(url) {
+			return fmt.Errorf("ERROR: '%s' is not a valid webhook token", args[0])
+		}
+		resp_json, err := requests.Get(url)
+		ManageError(err)
+		var resp_map map[string]interface{}
+		json.Unmarshal([]byte(resp_json.JSON()), &resp_map)
+
+		flags_map_bool := []bool{
 			has_avatar_url, is_bot, discriminator, author_id, username_content,
 			message_content, message_id, channel_id,
 			mentions_everyone, mentions_roles, is_pinned, timestamp, has_tts,
 			webhook_id, webhook_type,
 			components, edited_timestamp, embeds, flags,
 		}
-		flags_var := []string{
+		flags_map_var := []string{
 			"avatar", "bot", "discriminator", "id", "username",
 			"content", "id", "channel_id",
 			"mention_everyone", "mention_roles", "pinned", "timestamp", "tts",
 			"webhook_id", "wh_type",
 			"components", "edited_timestamp", "embeds", "flags",
 		}
+		var is_flag_set bool
 
-		url := fmt.Sprintf("%s/messages/%s", args[0], args[1])
-		if !is_token_valid(url) {
-			fmt.Printf("ERROR: '%s' is not a valid webhook token", args[0])
-		}
-
-		resp_json, err := requests.Get(url)
-		ManageError(err)
-		var resp_map map[string]interface{}
-		json.Unmarshal([]byte(resp_json.JSON()), &resp_map)
-
-		// Checks for each flag
-		var i int
-		var isFlagSet bool
-		for i = 0; i < len(flags); i++ {
-			if flags[i] {
-				if i >= 0 && i <= 4 {
-					// checks if flag is on map:author
-					resp_map_author := resp_map["author"]
-					output := resp_map_author.(map[string]interface{})[flags_var[i]]
-					output_type := reflect.TypeOf(output).String()
-					if output_type == "bool" {
-						output = fmt.Sprintf("%v", output)
-					}
-
-					fmt.Printf("%s: %s\n", flags_var[i], output)
-					isFlagSet = true
-
-				} else {
-					output := resp_map[flags_var[i]]
-					output_type := reflect.TypeOf(output).String()
-					if output_type == "bool" {
-						output = fmt.Sprintf("%v", output)
-					}
-
-					fmt.Printf("%s: %s\n", flags_var[i], output)
-					isFlagSet = true
-					// lol
+		for i := 0; i < len(flags_map_bool); i++ {
+			if flags_map_bool[i] {
+				output := resp_map[flags_map_var[i]]
+				output_type := reflect.TypeOf(output).String()
+				if output_type == "bool" {
+					output = fmt.Sprintf("%v", output)
 				}
-			} else {
-				continue
+
+				fmt.Printf("%s: %s\n", flags_map_var[i], output)
+				is_flag_set = true // lol
 			}
 		}
 
 		// default if no flag is set
-		if !isFlagSet {
-			output := resp_map["content"]
-			fmt.Println(output)
+		if !is_flag_set {
+			fmt.Println(resp_map)
 		}
+		return nil
 	},
 }
